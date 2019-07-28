37831add84aab52e26d0cb5bbf624fdb823b2b5e
Helpdesk <Helpdesk@procad278>
2019-07-28 18:40:20 -0500

    Prueba del método SELECT

--- src/manejopersonas/ManejoPersonas.java
+++ src/manejopersonas/ManejoPersonas.java
@@ -9,22 +9,22 @@ public class ManejoPersonas {
     public static void main(String[] args) {
         PersonasJDBC personasJDBC = new PersonasJDBC();
 	//Prueba del metodo insert
-        personasJDBC.insert("Alberto", "Juarez");
+        //personasJDBC.insert("Alberto", "Juarez");
         
         //Prueba del metodo update
-        personasJDBC.update(2, "Nombre3", "Apellido3");
+        //personasJDBC.update(2, "Nombre3", "Apellido3");
 		
         //Prueba del metodo delete
-        personasJDBC.delete(1);
+        //personasJDBC.delete(1);
       
         //Prueba del metodo select
         //Uso de un objeto persona para encapsular la informacion
         //de un registro de base de datos
-        //List<Personas> personas = personasJDBC.select();
-        //for (Personas persona : personas) {
-          //  System.out.print(persona);
-            //System.out.println("");
-       // }
+        List<Personas> personas = personasJDBC.select();
+            for (Personas persona : personas){
+                System.out.print(persona);
+                System.out.println("");
+            }
+       }
     }
 
-}
