e4fe83f4947775047c9ed863a36c7acf76cd50b6
Helpdesk <Helpdesk@procad278>
2019-07-28 18:16:14 -0500

    Esta es la clase que contiene los métodos para poder operar directamente con la tabla personas, El primer código es una prueba del método INSERT.

--- src/manejopersonas/ManejoPersonas.java
+++ src/manejopersonas/ManejoPersonas.java
@@ -1,9 +1,30 @@
-
 package manejopersonas;
 
-public class ManejoPersonas {
-    public static void main(String[] args) {
+import datos.PersonasJDBC;
+import domain.Personas;
+import java.util.List;
 
+public class ManejoPersonas {
+
+    public static void main(String[] args) {
+        PersonasJDBC personasJDBC = new PersonasJDBC();
+	//Prueba del metodo insert
+        personasJDBC.insert("Alberto", "Juarez");
+        
+        //Prueba del metodo update
+        //personasJDBC.update(2, "Nombre3", "Apellido3");
+		
+        //Prueba del metodo delete
+        //personasJDBC.delete(1);
+      
+        //Prueba del metodo select
+        //Uso de un objeto persona para encapsular la informacion
+        //de un registro de base de datos
+        //List<Personas> personas = personasJDBC.select();
+        //for (Personas persona : personas) {
+          //  System.out.print(persona);
+            //System.out.println("");
+       // }
     }
-    
+
 }
